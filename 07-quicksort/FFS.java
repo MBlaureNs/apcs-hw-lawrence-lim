e1437ae90494ec12c7366f554433b66f3fd6ec27
OokamiNya <42chrono@gmail.com>
2014-03-15 09:52:56 -0400

    wow, forgot to git add

---
+++ 07-quicksort/FFS.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class FFS {
+    //ArrayList<Integer> to int[]. ffs, java.
+    public int[] IALtoia(ArrayList<Integer> a) {
+	int[] r = new int[a.size()];
+	for(int i=0; i<a.size(); i++) {
+	    r[i] = a.get(i);
+	}
+	return r;
+    }
+}
